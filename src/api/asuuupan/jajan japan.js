7d629844dadbe9bb2ecfc0b401f12e1580f614fa
User15363828181728272 <defanaldrian1329@gmail.com>
2026-03-05 18:38:36 +0700

    Update jajan japan.js

--- src/api/asuuupan/jajan japan.js
+++ src/api/asuuupan/jajan japan.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 
 /**
- * 👩‍🦰 RANDOM CECAN KOREA
- * Path: /v2/asupan/korea
+ * 👩‍🦰 RANDOM CECAN JAPAN
+ * Path: /v2/asupan/japan
  * Creator: D2:业
  */
 
 module.exports = function (app) {
-    async function getCecanKorea() {
+    async function getCecanJapan() {
         // 1. Ambil list URL dari Pastebin
         const { data } = await axios.get('https://pastebin.com/raw/j9Hrx7V4', {
             headers: { 'User-Agent': 'Mozilla/5.0' }
@@ -15,13 +15,13 @@ module.exports = function (app) {
 
         const json = typeof data === 'string' ? JSON.parse(data) : data;
 
-        // Cek kategori korea
-        if (!json.korea || !Array.isArray(json.korea)) {
-            throw new Error('Kategori Korea gak ketemu mbut! 🗿');
+        // Cek kategori japan
+        if (!json.japan || !Array.isArray(json.japan)) {
+            throw new Error('Kategori Japan gak ketemu mbut! 🗿');
         }
 
         // 2. Pilih URL secara acak
-        const randomUrl = json.korea[Math.floor(Math.random() * json.korea.length)];
+        const randomUrl = json.japan[Math.floor(Math.random() * json.japan.length)];
         
         // 3. Ambil gambarnya dalam bentuk Buffer
         const response = await axios.get(randomUrl, { 
@@ -32,11 +32,11 @@ module.exports = function (app) {
         return Buffer.from(response.data);
     }
 
-    app.get("/v2/asupan/korea", async (req, res) => {
+    app.get("/v2/asupan/japan", async (req, res) => {
         try {
-            const buffer = await getCecanKorea();
+            const buffer = await getCecanJapan();
             
-            // Render langsung jadi gambar
+            // Render langsung jadi gambar di browser
             res.set('Content-Type', 'image/jpeg');
             res.send(buffer);
         } catch (err) {
